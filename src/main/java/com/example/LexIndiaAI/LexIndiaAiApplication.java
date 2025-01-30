db5ab63a536db84b44f961e6b8927a502b602595
aleempasha2772 <aleempasha1410@gmail.com>
2025-01-30 13:29:04 +0530

    Inital Commit

---
+++ src/main/java/com/example/LexIndiaAI/LexIndiaAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LexIndiaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexIndiaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexIndiaAiApplication.class, args);
+	}
+
+}
